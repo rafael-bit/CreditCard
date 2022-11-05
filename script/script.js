e6d53a708a56f7fc74fccb8ac1f531ac948f4417
rafael-bit <raquila743@gmail.com>
2022-11-05 14:27:56 -0300

    finish

--- script/script.js
+++ script/script.js
@@ -1,42 +1,149 @@
-$('.input-cart-number').on('keyup change', function () {
-	$t = $(this);
+let nameCard = document.querySelector(".card__details-name");
+let nameInput = document.querySelector("#cardholder");
+let nameErrorDiv = document.querySelector(".form__cardholder-error");
+let numberCard = document.querySelector(".card__number");
+let numberInput = document.querySelector("#cardNumber");
+let numberErrorDiv = document.querySelector('.form__inputnumber--error');
+let monthCard = document.querySelector(".card__month");
+let monthInput = document.querySelector("#cardMonth");
+let monthError = document.querySelector('.form__input-mm--error');
+let yearCard = document.querySelector(".card__year");
+let yearInput = document.querySelector("#cardYear");
+let yearError = document.querySelector('.form__input-yy--error');
+let cvcCard = document.querySelector(".card-back__cvc");
+let cvcInput = document.querySelector("#cardCvc");
+let cvcError = document.querySelector('.form__input-cvc--error');
 
-	if ($t.val().length > 3) {
-		$t.next().focus();
+nameInput.addEventListener("input", () => {
+	if (nameInput.value == '') {
+		nameCard.innerText = 'JANE APPLESEED';
+	} else {
+		nameCard.innerText = nameInput.value;
+	}
+});
+
+numberInput.addEventListener("input", e => {
+	let inputValue = e.target.value;
+	let regExp = /[A-z]/g;
+	if (regExp.test(numberInput.value)) {
+		showError(numberInput, numberErrorDiv, 'Wrong format, numbers only')
+	} else {
+		numberInput.value = inputValue.replace(/\s/g, "").replace(/([0-9]{4})/g, '$1 ').trim();
+		hiddeError(numberInput, numberErrorDiv);
+	}
+	if (numberInput.value == '') {
+		numberCard.innerText = '0000 0000 0000 0000';
+	} else {
+		numberCard.innerText = numberInput.value;
 	}
 
-	var card_number = '';
-	$('.input-cart-number').each(function () {
-		card_number += $(this).val() + ' ';
-		if ($(this).val().length == 4) {
-			$(this).next().focus();
+});
+
+monthInput.addEventListener('input', () => {
+	monthCard.innerText = monthInput.value;
+});
+
+yearInput.addEventListener('input', () => {
+	yearCard.innerText = yearInput.value;
+});
+
+cvcInput.addEventListener('input', () => {
+	cvcCard.innerText = cvcInput.value;
+	validateLetters(cvcInput, cvcError);
+});
+
+let confirmBtn = document.querySelector('.form__submit');
+let nameValidation = false;
+let numberValidation = false;
+let monthValidation = false;
+let yearValidation = false;
+let cvcValidation = false;
+let formSection = document.querySelector('.form');
+let thanksSection = document.querySelector('.thanks-section');
+
+confirmBtn.addEventListener('click', e => {
+	e.preventDefault();
+
+	if (verifyIsDilled(nameInput, nameErrorDiv)) {
+		nameValidation = true;
+	} else {
+		nameValidation = false;
+	}
+
+	if (verifyIsDilled(numberInput, numberErrorDiv) == true) {
+		if (numberInput.value.length == 19) {
+			hiddeError(numberInput, numberErrorDiv);
+			numberValidation = true;
+		} else {
+			showError(numberInput, numberErrorDiv, 'Wrong number');
+			numberValidation = false;
 		}
-	})
+	}
 
-	$('.credit-card-box .number').html(card_number);
+	verifyIsDilled(monthInput, monthError);
+	if (monthInput.value > 0 && monthInput.value <= 12) {
+		monthCard.innerText = monthInput.value;
+		hiddeError(monthInput, monthError);
+		monthValidation = true;
+
+	} else {
+		showError(monthInput, monthError, "Can't be blak");
+		monthValidation = false;
+	}
+
+	if (verifyIsDilled(yearInput, yearError)) {
+		if (parseInt(yearInput.value) > 21 && parseInt(yearInput.value) < 28) {
+			hiddeError(numberInput, numberErrorDiv);
+			yearValidation = true;
+		} else {
+			showError(yearInput, yearError, 'Wrong Year');
+			yearValidation = false;
+		}
+	}
+
+	if (verifyIsDilled(cvcInput, cvcError)) {
+		if (cvcInput.value.length == 3) {
+			hiddeError(cvcInput, cvcError);
+			cvcValidation = true;
+		} else {
+			showError(cvcInput, cvcError, 'Wrong cvc');
+			cvcValidation = false;
+		}
+	}
+
+	if (nameValidation == true && numberValidation == true && monthValidation == true && yearValidation == true && cvcValidation == true) {
+		formSection.style.display = 'none';
+		thanksSection.style.display = 'block';
+	}
 });
 
-$('#card-holder').on('keyup change', function () {
-	$t = $(this);
-	$('.credit-card-box .card-holder div').html($t.val());
-});
+function showError(inputError, divError, msgError) {
+	divError.innerText = msgError;
+	inputError.style.borderColor = '#FF0000';
+}
 
-$('#card-holder').on('keyup change', function () {
-	$t = $(this);
-	$('.credit-card-box .card-holder div').html($t.val());
-});
+function hiddeError(inputError, divError) {
+	divError.innerText = '';
+	inputError.style.borderColor = 'hsl(270, 3%, 87%)';
+}
 
-$('#card-expiration-month, #card-expiration-year').change(function () {
-	m = $('#card-expiration-month option').index($('#card-expiration-month option:selected'));
-	m = (m < 10) ? '0' + m : m;
-	y = $('#card-expiration-year').val().substr(2, 2);
-	$('.card-expiration-date div').html(m + '/' + y);
-})
+function verifyIsDilled(divInput, divError) {
+	if (divInput.value.length > 0) {
+		hiddeError(divInput, divError);
+		return true;
+	} else {
+		showError(divInput, divError, "Can't be blank");
+		return false;
+	}
+}
 
-$('#card-ccv').on('focus', function () {
-	$('.credit-card-box').addClass('hover');
-}).on('blur', function () {
-	$('.credit-card-box').removeClass('hover');
-}).on('keyup change', function () {
-	$('.ccv div').html($(this).val());
-});
+function validateLetters(input, error) {
+	let regExp = /[A-z]/g;
+	if (regExp.test(input.value)) {
+		showError(input, error, 'Wrong format, numbers only')
+	} else {
+		hiddeError(input, error);
+	}
+}
+
+
